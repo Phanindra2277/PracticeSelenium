249f4f3bca1d181afaf25d77a4645bb87081f10b
Phanindra2277 <phanindrakantheti664@gmail.com>
2025-07-12 10:07:50 +0530

    checkboxes

--- src/test/java/checkBoxesAndRadioBtn/Checkboxes.java
+++ src/test/java/checkBoxesAndRadioBtn/Checkboxes.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Checkboxes {
 
@@ -20,7 +21,7 @@ public class Checkboxes {
 		driver.get("https://phppot.com/demo/jquery-dependent-dropdown-list-countries-and-states/");
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("//select[@id='country-list']")).click();
+		/*driver.findElement(By.xpath("//select[@id='country-list']")).click();
 		
 		List<WebElement> countrylist=driver.findElements(By.xpath("//select[@id='country-list']//option"));
 		
@@ -31,9 +32,9 @@ public class Checkboxes {
 			}
 		}
 		
-		driver.findElement(By.xpath("//select[@id='state-list']")).click();
+		driver.findElement(By.xpath("//select[@name='state']")).click();
 		
-		List<WebElement> statelist=driver.findElements(By.xpath("//select[@id='state-list' and @name='state']//option"));
+		List<WebElement> statelist=driver.findElements(By.xpath("//select[@name='state']//option"));
 		
 		System.out.println(statelist.size());
 		for (WebElement opt:statelist) {
@@ -41,9 +42,26 @@ public class Checkboxes {
 			if(opt.getText().equals("Andhra Pradesh")) {
 				opt.click();
 			}
-		}
+		}*/
 		
 		
+		
+		//using with select class
+		
+		WebElement country=driver.findElement(By.xpath("//select[@id='country-list']"));		
+		
+		Select countryopt=new Select(country);
+		List<WebElement> opt=countryopt.getOptions();
+		System.out.println(opt.size());
+		countryopt.selectByVisibleText("India");
+		
+		WebElement state=driver.findElement(By.xpath("//select[@name='state']"));
+		
+		Select stateopt=new Select(state);
+		List<WebElement> opts=stateopt.getOptions();
+		System.out.println(opts.size());
+		
+		stateopt.selectByVisibleText("Andhra Pradesh");
 		
 		Thread.sleep(3000);
 		driver.quit();
